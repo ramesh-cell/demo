11d09411f416bbe41ebe6ce35e7584669e46b5e2
ramesh-cell <70745319+ramesh-cell@users.noreply.github.com>
2022-06-26 21:57:18 +0530

    20001 controllers services added (#2)
    * added mongo configurations
    
    * added mongo services

--- src/main/java/com/inetsolv/demo/DemoApplication.java
+++ src/main/java/com/inetsolv/demo/DemoApplication.java
@@ -2,8 +2,11 @@ package com.inetsolv.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories("com.inetsolv.demo.repositories")
 public class DemoApplication {
 
 	public static void main(String[] args) {
