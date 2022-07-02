44c575187453350a175d9056d71251656f8be083
ramesh <rameshjava4plus@gmail.com>
2022-07-02 23:26:45 +0530

    exceptions added

--- src/main/java/com/inetsolv/demo/DemoApplication.java
+++ src/main/java/com/inetsolv/demo/DemoApplication.java
@@ -12,6 +12,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
              System.out.println("//V3");
+
+
+
 	}
 
 }
