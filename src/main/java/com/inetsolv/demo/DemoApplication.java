55b9ce60d084a4c121e422cc1fd19b0dbb36b87f
ramesh <rameshjava4plus@gmail.com>
2022-07-02 20:07:28 +0530

    Hello from local 1001_test change

--- src/main/java/com/inetsolv/demo/DemoApplication.java
+++ src/main/java/com/inetsolv/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-                  System.out.println("Hello from local 1001_test change v1");
+                  System.out.println("Hello from local 1001_test change");
           
             
 	}
