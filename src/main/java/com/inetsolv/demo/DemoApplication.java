536d65214fec952b324f14c20f2d37efef1ea22e
ramesh-cell <70745319+ramesh-cell@users.noreply.github.com>
2022-07-02 19:35:54 +0530

    change place sysout

--- src/main/java/com/inetsolv/demo/DemoApplication.java
+++ src/main/java/com/inetsolv/demo/DemoApplication.java
@@ -11,8 +11,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
-            System.out.println("removed Hello");
+                  System.out.println("removed Hello");
+          
             
 	}
 
